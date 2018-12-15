8f8d6dc46ba4f7cfa015e7e8bf4ab626060710f0
Lior Kamrat <kamrat9@gmail.com>
2018-12-14 16:44:46 -0800

    initial commit

---
+++ vars.tf
@@ -0,0 +1,10 @@
+# Declare Azure variables
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "location" {
+    default = "West US2"
+}
+variable "admin_username" {}
+variable "admin_password" {}
